735a73d10a9f47af53df6b6256465d38c0a75af7
superseva <superseva@gmail.com>
2020-10-25 11:55:39 +0100

    Roller Functional

--- module/components/dice-roller.js
+++ module/components/dice-roller.js
@@ -4,28 +4,8 @@ export class DiceRoller {
         let rollFormula = `${num}db + 1dk`;
         let r = new Roll(rollFormula);
         r.roll();
-        console.warn(r);
-
-        // if num==2
-        //get TOTAL
-        console.warn(`TOTAL = ${r.total}`);
-        // get num of duplicates
-        let diceResults = [];
-        r.dice[0].results.forEach((element) => {
-            diceResults.push(element.result);
-        });
-        diceResults.push(r.dice[1].results[0].result);
-        let dup = DiceRoller.findMaxDuplicates(diceResults);
-        console.warn(`DUPLICATES = ${dup}`, dup);
-        DiceRoller.SendToChat(r);
-
-        // -----------------------
-        //if num==3
-        //get all that are bigger than diff {DIFF ???}
-        //get the one with most dupicates
+        DiceRoller.sendToChat(r);
     }
-
-    _evaluateResult(array) {}
 
     static findMaxDuplicates(array) {
         var counts = {};
@@ -36,15 +16,52 @@ export class DiceRoller {
         return counts;
     }
 
-    static async SendToChat(_roll) {
-        let rollData = {};
+    static countDuplicates(arr) {
+        var count = {};
+        arr.forEach(function (i) {
+            count[i] = (count[i] || 0) + 1;
+        });
+        console.warn(count);
+        let maxDup = Object.keys(count).reduce((a, b) => (count[a] > count[b] ? a : b));
+        console.warn(maxDup);
+        if (count[maxDup] == 1) return 0;
+        else return count[maxDup];
+        //return maxDup;
+    }
+
+    static async sendToChat(r) {
+        let baseDice = [];
+        let diceResults = [];
+        r.dice[0].results.forEach((element) => {
+            baseDice.push(element.result);
+        });
+        diceResults = [...baseDice];
+        diceResults.push(r.dice[1].results[0].result);
+        //let dup = DiceRoller.findMaxDuplicates(diceResults);
+        let dup = DiceRoller.countDuplicates(diceResults);
+        let total = baseDice.length < 3 ? diceResults.reduce((acc, res) => acc + res, 0) : "?";
+        let discardable = baseDice.length < 3 ? null : true;
+        /*if (baseDice.length < 3) {
+            total = diceResults.reduce((acc, res) => acc + res, 0);
+        } else {
+            discardable = true;
+        }*/
+        let rollData = {
+            baseDice: baseDice,
+            kismetDice: r.dice[1].results[0].result,
+            diceResults: diceResults,
+            duplicates: baseDice.length < 3 ? dup : "?",
+            total: total,
+            discardable: discardable,
+        };
+        console.warn(rollData);
         const html = await renderTemplate("systems/talisman/templates/chat/roll.html", rollData);
         let chatData = {
             user: game.user._id,
             rollMode: game.settings.get("core", "rollMode"),
             content: html,
             type: CHAT_MESSAGE_TYPES.ROLL,
-            roll: _roll,
+            roll: r,
         };
         if (["gmroll", "blindroll"].includes(chatData.rollMode)) {
             chatData.whisper = ChatMessage.getWhisperRecipients("GM");
@@ -52,5 +69,32 @@ export class DiceRoller {
             chatData.whisper = [game.user];
         }
         await ChatMessage.create(chatData);
+    }
+
+    static updateMessage(message, html, el) {
+        console.log(`updating: ${message.id} with el`);
+        console.log(el.currentTarget.dataset);
+        let ignoreDieIndex = el.currentTarget.dataset.index;
+        let diceResults = [];
+        let baseDice = message.roll.dice[0].results.map((i) => i.result);
+        baseDice.splice(ignoreDieIndex, 1);
+        diceResults = [...baseDice];
+        diceResults.push(message.roll.dice[1].results[0].result);
+        //let dup = DiceRoller.findMaxDuplicates(diceResults);
+        let dup = DiceRoller.countDuplicates(diceResults);
+        let total = diceResults.reduce((acc, res) => acc + res, 0);
+        //console.log(diceResults, total, dup);
+        //update DUPLICATES
+        html.find(".label-success").html(dup);
+        //update TOTAL
+        html.find(".label-total").html(total);
+        //update opacity
+        $(el.currentTarget).addClass("discarded");
+        let diceImages = html.find(".dice-image");
+        diceImages.each((i, e) => {
+            if (el.currentTarget.dataset.index != e.dataset.index) {
+                $(e).removeClass("discarded");
+            }
+        });
     }
 }
--- module/talisman.js
+++ module/talisman.js
@@ -9,3 +9,45 @@ Hooks.once("init", async function () {
     CONFIG.Dice.terms["b"] = TalismanDieBase;
     CONFIG.Dice.terms["k"] = TalismanDieKismet;
 });
+
+Hooks.on("renderChatMessage", (message, html) => {
+    html.find(".dice-discardable").click((el) => {
+        DiceRoller.updateMessage(message, html, el);
+    });
+});
+
+/* -------------------------------------------- */
+/*  DsN Hooks                                   */
+/* -------------------------------------------- */
+
+Hooks.on("diceSoNiceRollComplete", (chatMessageID) => {});
+
+Hooks.once("diceSoNiceReady", (dice3d) => {
+    dice3d.addSystem({ id: "talisman", name: "Talisman" }, true);
+    dice3d.addDicePreset({
+        type: "db",
+        labels: [
+            "systems/talisman/ui/dice/tdb1.png",
+            "systems/talisman/ui/dice/tdb2.png",
+            "systems/talisman/ui/dice/tdb3.png",
+            "systems/talisman/ui/dice/tdb4.png",
+            "systems/talisman/ui/dice/tdb5.png",
+            "systems/talisman/ui/dice/tdb6.png",
+        ],
+        colorset: "black",
+        system: "talisman",
+    });
+    dice3d.addDicePreset({
+        type: "dk",
+        labels: [
+            "systems/talisman/ui/dice/tdk1.png",
+            "systems/talisman/ui/dice/tdk2.png",
+            "systems/talisman/ui/dice/tdk3.png",
+            "systems/talisman/ui/dice/tdk4.png",
+            "systems/talisman/ui/dice/tdk5.png",
+            "systems/talisman/ui/dice/tdk6.png",
+        ],
+        colorset: "black",
+        system: "talisman",
+    });
+});
